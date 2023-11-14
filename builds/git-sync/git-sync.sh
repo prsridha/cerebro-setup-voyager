bd545556aa4d12cb642d579db6e6cb195dbe9f01
prsridha <prsridha@ucsd.edu>
2023-11-13 22:35:57 -0800

    removed git secrets from git-sync base build

--- builds/git-sync/git-sync.sh
+++ builds/git-sync/git-sync.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# create .ssh dir under home and copy the git credentials from /etc/git-secret
-mkdir $HOME/.ssh
-cp /etc/git-secret/* $HOME/.ssh/
-mv $HOME/.ssh/ssh $HOME/.ssh/id_rsa.git
-touch $HOME/.ssh/config
-ssh-keyscan -t rsa $GIT_SYNC_SERVER >> ~/.ssh/known_hosts
-
-# create git config file in .ssh
-echo "
-Host $GIT_SYNC_SERVER
-    Hostname $GIT_SYNC_SERVER
-    IdentityFile $HOME/.ssh/id_rsa.git
-    IdentitiesOnly yes
-" > $HOME/.ssh/config
-
 # clone the repo in the required directory
 mkdir -p $GIT_SYNC_ROOT
 cd $GIT_SYNC_ROOT
